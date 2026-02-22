157b48ee147aa0d55df3ba83c9058bc6e33dbeab
hridik-suresh <iamhridiksuresh@gmail.com>
2026-02-22 14:42:36 +0530

    feat: Connection between the react and redux is established.
    Now react will make use of the reduc store.
    Also added Toaster support for the notification.

--- nanolink-frontend/src/main.jsx
+++ nanolink-frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { Toaster } from "react-hot-toast";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+      <Toaster position="top-center" reverseOrder={false} />
+    </Provider>
   </StrictMode>,
-)
+);
